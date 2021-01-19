83a08492eaadd634f2715fb9add492509ad30bb5
Abdulaziz Alnaim <45672672+211513441@users.noreply.github.com>
2021-01-19 07:56:42 +0300

    Add files via upload

---
+++ LAB2/Lab2/src/AbstractProblem.java
@@ -0,0 +1,11 @@
+
+public class AbstractProblem {
+    public static void main(String[] args) {
+        
+        Athlete ath=new Athlete(1,1000,"Ali",321);
+        ath.displayDetails();
+        
+        
+    }
+    
+}
